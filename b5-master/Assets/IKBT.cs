e64037c3139034d0237d548dd3b78463ff6224b3
vr266 <vr266@scarletmail.rutgers.edu>
2020-04-20 22:18:00 -0400

    Initial commit

---
+++ b5-master/Assets/IKBT.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TreeSharpPlus;
+using RootMotion.FinalIK;
+
+public class IKBT : MonoBehaviour
+{
+    public Transform wander1;
+    public Transform wander2;
+    public Transform wander3;
+    public GameObject participant;
+
+    //IK related interface
+    public GameObject ball;
+    public InteractionObject ikBall;
+    public FullBodyBipedEffector hand;
+
+
+    private BehaviorAgent behaviorAgent;
+    // Use this for initialization
+    void Start()
+    {
+        behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(behaviorAgent);
+        behaviorAgent.StartBehavior();
+    }
+
+    #region IK related function
+    protected Node PickUp(GameObject p)
+    {
+        return new Sequence(this.Node_BallStop(),
+                            p.GetComponent<BehaviorMecanim>().Node_StartInteraction(hand, ikBall),
+                            new LeafWait(1000),
+                            p.GetComponent<BehaviorMecanim>().Node_StopInteraction(hand));
+    }
+
+    public Node Node_BallStop()
+    {
+        return new LeafInvoke(() => this.BallStop());
+    }
+    public virtual RunStatus BallStop()
+    {
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+
+        return RunStatus.Success;
+    }
+
+    protected Node PutDown(GameObject p)
+    {
+        return new Sequence(p.GetComponent<BehaviorMecanim>().Node_StartInteraction(hand, ikBall),
+                            new LeafWait(300),
+                            this.Node_BallMotion(),
+                            new LeafWait(500), p.GetComponent<BehaviorMecanim>().Node_StopInteraction(hand));
+    }
+
+    public Node Node_BallMotion()
+    {
+        return new LeafInvoke(() => this.BallMotion());
+    }
+
+    public virtual RunStatus BallMotion()
+    {
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = false;
+        ball.transform.parent = null;
+        return RunStatus.Success;
+    }
+    #endregion
+
+    protected Node ST_ApproachAndWait(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(participant.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        Node roaming = new Sequence(
+                        this.ST_ApproachAndWait(this.wander1),
+                        new DecoratorLoop(
+                            new Sequence(this.PickUp(participant), this.PutDown(participant)))
+                        );
+        return roaming;
+    }
+}
